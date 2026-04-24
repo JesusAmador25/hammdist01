fc668206128565b8ad122663470681d555c30159
Jesus Amador Mendoza <am378397@uaeh.edu.mx>
2026-04-24 00:06:25 +0000

    changes in pages

--- src/hammindist01/solvers.py
+++ src/hammindist01/solvers.py
@@ -1,6 +1,6 @@
 def solve_example(input_data):
     """
-    A placeholder solver following the backtracking pattern.
+    A placeholder solver following the backtracking pattern. This is a test to see the changes in the git-pages
 
     Args:
         input_data (list): The problem constraints.
